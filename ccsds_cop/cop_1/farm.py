aa96aec957ae30df3f93eeb5186f90dfa287445c
Andrew Stanton <81195290+sneakyaardvark@users.noreply.github.com>
2026-06-06 11:21:54 -0700

    fix: vcf_count header check doesn't explicitly check for None

--- ccsds_cop/cop_1/farm.py
+++ ccsds_cop/cop_1/farm.py
@@ -283,7 +283,7 @@ class Farm1(CopService):
             if frame.header.prot_ctrl_cmd_flag != ProtocolCommandFlag.USER_DATA:
                 logger.error("Discarding frame (E9): invalid 'Type-AC' frame")
                 return False
-            if not frame.header.vcf_count:
+            if frame.header.vcf_count is None:
                 raise TypeError("Header does not contain a VCF count")
             ns: int = frame.header.vcf_count
             if ns == self.v_r:
